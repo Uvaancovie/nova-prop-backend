4929ede31475b8be01b9b51ee74ac1c5613f2e29
GVNDAN011 <ST10022006@VCCONNECT.EDU.ZA>
2025-12-19 12:22:26 +0200

    the email is info@nova-prop.com

--- controllers/waitlistController.js
+++ controllers/waitlistController.js
@@ -115,7 +115,7 @@ exports.joinWaitlist = async (req, res) => {
     try {
       await resend.emails.send({
         from: 'PropStream Waitlist <onboarding@nova-prop.com>',
-        to: ['info@novaprop.com'],
+        to: ['info@nova-prop.com'],
         subject: '🎯 New Waitlist Signup',
         html: `
           <!DOCTYPE html>
